17b3acb93beb419b30b73a7418daf60df79f40b0
grach417 <grach417@gmail.com>
2019-03-16 01:17:39 +0500

    Пробная реализация объединения.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,55 @@
 package com.company;
 
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
- public static void main(String[] args) {
-    Persons Anatoliy = new Persons(180, 71.2f);
-    Anatoliy.say();
-    System.out.println(Anatoliy.hight);
-    Student Polina = new Student(179, 69.9f, 1);
-     System.out.println(Polina.course);
+    public static void main(String[] args) {
+
+
+        int n;
+        Scanner scar = new Scanner(System.in);
+
+        System.out.println("Введите количество цифр в массивах: ");
+
+        n = scar.nextInt();
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int[] c = new int[n];
+
+        String cString = Arrays.toString(c);
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.print("Введите а[" + i + "] =  ");
+            a[i] = scar.nextInt();
+
+            if (a[i] <= 1 && a[i] >= 0) {
+
+                System.out.print("Введите b[" + i + "] =  ");
+
+                b[i] = scar.nextInt();
+
+                if (b[i] > 1 && b[i] < 0) {
+                    System.out.println("Error");
+                    break;
+                }
+
+            } else {
+                System.out.println("Error");
+                break;
+            }
+            c = addElement(c, a[i] * b[i]);
+        }
+        System.out.println(cString);
+    }
+    private static int[] addElement(int[] a, int e) {
+        a  = Arrays.copyOf(a, a.length + 1);
+        a[a.length - 1] = e;
+        return a;
     }
 
 }
-
