7ce30735ec907ef1e3c685af7b18c4df9eea2793
grach417 <grach417@gmail.com>
2019-03-19 02:27:05 +0500

    /

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,54 +2,37 @@ package com.company;
 
 
 import java.util.Arrays;
-import java.util.Scanner;
+
+
+import static java.util.Arrays.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        int[] a = new int[]{1,0,1};
+        int[] b = new int[]{1,1,1};
+        int[] c = new int[3];
 
+        int nextInt = 0;
 
-        int n;
-        Scanner scar = new Scanner(System.in);
-
-        System.out.println("Введите количество цифр в массивах: ");
-
-        n = scar.nextInt();
-
-        int[] a = new int[n];
-        int[] b = new int[n];
-        int[] c = new int[n];
-
-        String cString = Arrays.toString(c);
-
-        for (int i = 0; i < n; i++) {
-
-            System.out.print("Введите а[" + i + "] =  ");
-            a[i] = scar.nextInt();
-
-            if (a[i] <= 1 && a[i] >= 0) {
-
-                System.out.print("Введите b[" + i + "] =  ");
-
-                b[i] = scar.nextInt();
-
-                if (b[i] > 1 && b[i] < 0) {
-                    System.out.println("Error");
-                    break;
-                }
-
-            } else {
-                System.out.println("Error");
-                break;
-            }
-            c = addElement(c, a[i] * b[i]);
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] * b[i];
         }
-        System.out.println(cString);
-    }
-    private static int[] addElement(int[] a, int e) {
-        a  = Arrays.copyOf(a, a.length + 1);
-        a[a.length - 1] = e;
-        return a;
-    }
+        System.out.println(Arrays.toString(c));
 
+
+        int[] both = copyOf(a, a.length + b.length);
+        System.arraycopy(b, 0, both, a.length, b.length);
+        System.out.println(Arrays.toString(both));
+
+
+        int[] f = removeElement(a, 2);
+
+        System.out.println(Arrays.toString(f));
+
+    }
+    private static int[] removeElement(int[] n, int i) {
+        System.arraycopy(n, i + 1, n, i, n.length - 1 - i);
+        return n;
+    }
 }
